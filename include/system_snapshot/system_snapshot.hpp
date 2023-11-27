15390e10ddd294eb8a48f8569d2b3e606d4294e1
Ruben Laso <laso@par.tuwien.ac.at>
2023-11-27 13:57:17 +0100

    feat: approximated load per process (intended to be as close as possible to what is shown in /proc/loadavg)

--- include/system_snapshot/system_snapshot.hpp
+++ include/system_snapshot/system_snapshot.hpp
@@ -28,8 +28,11 @@ namespace system_snapshot
 		std::vector<fast_uset<pid_t>> node_pid_map_; // input: node, output: list of TIDs
 
 		// Cache of the CPU and node of each PID
-		fast_umap<pid_t, int> pid_cpu_map_;  // input: TID,  output: CPU
-		fast_umap<pid_t, int> pid_node_map_; // input: TID,  output: node
+		fast_umap<pid_t, int> pid_cpu_map_;  // input: TID, output: CPU
+		fast_umap<pid_t, int> pid_node_map_; // input: TID, output: node
+
+		// Load of each PID
+		fast_umap<pid_t, float> pid_load_map_; // input: TID, output: load
 
 		std::vector<float> cpu_use_;  // input: CPU,  output: use
 		std::vector<float> node_use_; // input: node, output: use
@@ -49,6 +52,73 @@ namespace system_snapshot
 
 		fast_umap<pid_t, int> cpu_migrations_;  // input: PID, output: destination CPU
 		fast_umap<pid_t, int> node_migrations_; // input: PID, output: destination node
+
+		template<typename Map>
+		void compute_load_sigmoid(const Map & pid_usage_map)
+		{
+			static const auto load = [](const float cpu_use, const float slice) {
+				return std::min(1.0F, cpu_use / slice);
+			};
+
+			// S-shape function between 0 and 1
+			static const auto weight = [](const float x) {
+				// If x ~ 0, return 0
+				if (x < std::numeric_limits<typeof(x)>::epsilon()) { return 0.0F; }
+				// If x ~ 1, return 1
+				if (x > 1.0F - std::numeric_limits<typeof(x)>::epsilon()) { return 1.0F; }
+				// Else, compute the function
+				constexpr auto beta = 3.0F;
+				return 1.0F / (1.0F + std::pow((x / (1.0F - x)), -beta));
+			};
+
+			// Generate a vector of 101 values such that v[i] = s_shape(i / 100);
+			static const auto weight_table = []() {
+				std::array<float, 101> table{};
+
+				for (size_t i = 0; i < table.size(); i++)
+				{
+					table.at(i) = weight(static_cast<float>(i) / 100.0F);
+				}
+
+				return table;
+			}();
+
+			const auto free_cpu_use =
+			    std::clamp(100.0F - ranges::accumulate(pid_usage_map | ranges::views::values, 0.0F), 0.0F, 100.0F);
+			const auto max_cpu_use = ranges::max(pid_usage_map | ranges::views::values);
+
+			const auto alpha = weight_table[static_cast<size_t>(free_cpu_use)];
+			const auto beta  = 1 - alpha;
+
+			for (const auto & [pid, cpu_use] : pid_usage_map)
+			{
+				const auto load_vs_free = load(cpu_use, free_cpu_use);
+				const auto load_vs_max  = load(cpu_use, max_cpu_use);
+
+				const auto pid_load = alpha * load_vs_free + beta * load_vs_max;
+
+				pid_load_map_[pid] = pid_load;
+			}
+		}
+
+		void compute_loads(const int cpu)
+		{
+			const auto & pids = cpu_pid_map_.at(cpu);
+
+			auto pid_usage_map = pids | ranges::views::transform([&](const auto pid) {
+				                     return std::pair<pid_t, float>{ pid, processes_.cpu_use(pid) };
+			                     });
+
+			compute_load_sigmoid(pid_usage_map);
+		}
+
+		void compute_loads()
+		{
+			for (const auto cpu : topology_.cpus())
+			{
+				compute_loads(cpu);
+			}
+		}
 
 		void rebuild()
 		{
@@ -88,6 +158,8 @@ namespace system_snapshot
 
 			dirty_cpu_use_  = cpu_use_;
 			dirty_node_use_ = node_use_;
+
+			compute_loads();
 		}
 
 		void build()
@@ -211,6 +283,25 @@ namespace system_snapshot
 		[[nodiscard]] auto cpu_use(const int cpu) const { return cpu_use_.at(cpu); }
 
 		[[nodiscard]] auto node_use(const int node) const { return node_use_.at(node); }
+
+		[[nodiscard]] auto load_of(const pid_t pid) const { return pid_load_map_.at(pid); }
+
+		[[nodiscard]] auto load_of_cpu(const int cpu) const
+		{
+			return ranges::accumulate(
+			    pids_in_cpu(cpu) | ranges::views::transform([&](const auto pid) { return load_of(pid); }), 0.0F);
+		}
+
+		[[nodiscard]] auto load_of_node(const int node) const
+		{
+			return ranges::accumulate(
+			    pids_in_node(node) | ranges::views::transform([&](const auto pid) { return load_of(pid); }), 0.0F);
+		}
+
+		[[nodiscard]] auto load_system() const
+		{
+			return ranges::accumulate(pid_load_map_ | ranges::views::values, 0.0F);
+		}
 
 		void migrate_to_cpu(const pid_t pid, const int cpu)
 		{
